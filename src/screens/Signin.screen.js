1707944590e37e2dff7641491ee350645310fe58
evan fung <evanfungv@gmail.com>
2018-04-24 01:32:04 +0800

    feat: create signin screen

---
+++ src/screens/Signin.screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Signin extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Signin pages.</Text>
+      </View>
+    );
+  }
+}
+
+export default Signin;
