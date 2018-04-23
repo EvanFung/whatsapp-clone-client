e0a466671b7b558341f8ee2edaa6b11fef201746
evan fung <evanfungv@gmail.com>
2018-04-24 02:15:18 +0800

    chore: add loadFonts function in Appjs

--- src/screens/Signin.screen.js
+++ src/screens/Signin.screen.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Icon,
+  Body,
+  Left,
+  Right,
+  Item,
+  Input,
+  Form
+} from "native-base";
 
 class Signin extends Component {
   render() {
     return (
-      <View>
-        <Text>Signin pages.</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Rounded</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content padder>
+          <Form>
+            <Item rounded>
+              <Input placeholder="Rounded Textbox" />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
     );
   }
 }
