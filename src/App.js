d777ce85850418e7b828c6e011e2bbcfbbbbfa53
FENGWENHAO <evanfungv@gmail.com>
2018-05-04 02:39:13 +0800

    feat: create index reducers

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Signin from './screens/Signin.screen';
 import { StackNavigator, SwitchNavigator } from 'react-navigation';
 import Boot from './boot/setup';
 import { StyleSheet, View } from 'react-native';
-
+import { Provider } from 'react-redux';
 const AppNavigator = StackNavigator({
   Signin: { screen: Signin }
 });
@@ -12,9 +12,11 @@ const AppNavigator = StackNavigator({
 export default class App extends React.Component {
   render() {
     return (
-      <Root>
-        <AppNavigator />
-      </Root>
+      <Provider>
+        <Root>
+          <AppNavigator />
+        </Root>
+      </Provider>
     );
   }
 }
